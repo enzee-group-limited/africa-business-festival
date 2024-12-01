8457be5e5a1b04a6c5476543ca4ec5dcb6f364d2
BethTawiah <elizabeth.tawiah@amalitech.org>
2024-12-01 01:01:58 +0000

    countdown  feature

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,5 @@ import flyer from "./images/flyer.jpg"
 import flyer2 from "./images/flyer2.jpg"
 
 export {
-    homePageImgslide1,homePageImgslide2, whyUsbg, flyer,flyer2
+    homePageImgslide1,homePageImgslide2, whyUsbg, flyer,flyer2,
 }
